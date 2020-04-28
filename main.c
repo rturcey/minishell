9e924dc2630ce97e5dc378be570f4b38aced4b8e
rturcey <rturcey@student.42.fr>
2020-04-28 10:18:59 +0200

    prompt

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+
+//importer libft
+
+char	*find_value(char **env, const char *str)
+{
+	int		i;
+	int		j;
+	size_t	len;
+
+	i = -1;
+	len = strlen(str);
+	while(env[++i])
+	{
+		j = 0;
+		while(env[i][j] == str[j])
+		{
+			if (j == len - 1)
+				return(&env[i][len]);
+			j++;
+		}
+	}
+	return (NULL);
+}
+
+int		main(int argc, char **argv, char **env)
+{
+	char	*buff[101];
+	char	*path;
+
+	path = malloc(100);
+	//trouve le chemin courant pour le prompt:
+	getcwd(path, PATH_MAX);
+	//remplace le path de home/user par ~:
+	int		len = (int)strlen(find_value(env, "HOME="));
+	strcpy(&path[1], &path[len]);
+	path[0] = '~';
+	write(1, path, strlen(path));
+	write(1, " $> ", 4);
+	free(path);
+	//boucler gnl (read juste pour le test):
+	read(0, buff, 100);
+	return (0);
+}
