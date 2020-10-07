f8a04ea2cf6a6bab9bc6ca94e417dcfc8ee616ac
stella <estelle.slrd@gmail.com>
2020-10-07 11:22:26 +0200

    fixed gnl free

--- sources/gnl_ms.c
+++ sources/gnl_ms.c
@@ -6,14 +6,20 @@
 /*   By: esoulard <esoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 12:42:15 by esoulard          #+#    #+#             */
-/*   Updated: 2020/09/24 19:20:39 by esoulard         ###   ########.fr       */
+/*   Updated: 2020/10/07 11:20:47 by esoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdio.h>
 
-char	*give_it_back_ms(char *str, char **line)
+static int	free_sfd(char *str)
+{
+	free(str);
+	return (0);
+}
+
+char		*give_it_back_ms(char *str, char **line)
 {
 	int		len;
 	char	*result;
@@ -30,7 +36,7 @@ char	*give_it_back_ms(char *str, char **line)
 	return (result);
 }
 
-int		gnl_ms(int fd, char **line)
+int			gnl_ms(int fd, char **line)
 {
 	static char	*s[FOPEN_MAX];
 	char		buffer[31];
@@ -44,7 +50,7 @@ int		gnl_ms(int fd, char **line)
 	while (ft_strchr(s[fd], '\n') || (i = read(fd, buffer, 30)) >= 0)
 	{
 		if ((buffer[i] = '\0') == 0 && i == 0 && ft_strlen(s[fd]) == 0)
-			return (0);
+			return (free_sfd(s[fd]));
 		if (!(s[fd] = ft_gnl_strjoin(s[fd], buffer)))
 			return (-1);
 		if (ft_strchr(s[fd], '\n') && ++i)
